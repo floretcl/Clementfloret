12d4facd3f727b27be627128a00f7998d222c7b6
Floretcl <38957472+floretcl@users.noreply.github.com>
2024-05-16 18:27:51 +0200

    add send_email to contactForm

--- portfolio/forms.py
+++ portfolio/forms.py
@@ -1,4 +1,7 @@
 from django import forms
+from django.core.mail import send_mail
+
+from clementfloret.settings import env
 
 
 class ContactForm(forms.Form):
@@ -6,3 +9,12 @@ class ContactForm(forms.Form):
     email = forms.EmailField(label='Email', max_length=100, min_length=3, required=True)
     subject = forms.CharField(label='Sujet', max_length=200, min_length=5, required=True)
     message = forms.CharField(widget=forms.Textarea, label='Message', min_length=10, required=True)
+
+    def send_email(self):
+        send_mail(
+            subject=self.cleaned_data['subject'],
+            message=self.cleaned_data['message'],
+            from_email=self.cleaned_data['email'],
+            recipient_list=env('EMAIL_RECIPIENT_LIST')
+        )
+        pass
