6ebe0bc9f5989a8cb47816aa35eeb81b472e2c9f
Floretcl <38957472+floretcl@users.noreply.github.com>
2024-05-16 18:38:35 +0200

    clean up

--- clementfloret/settings.py
+++ clementfloret/settings.py
@@ -66,8 +66,7 @@ ROOT_URLCONF = 'clementfloret.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
