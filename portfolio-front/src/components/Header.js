78efc0a7a62e4abd49200b9ac59f31a7a44f8027
Floretcl <38957472+floretcl@users.noreply.github.com>
2024-05-30 23:31:39 +0200

    add Header component (mobile ok)

---
+++ portfolio-front/src/components/Header.js
@@ -0,0 +1,12 @@
+import Logo from "./Logo.js";
+import Menu from "./Menu.js";
+
+export default function Header() {
+
+    return (
+        <header className="header">
+            <Logo />
+            <Menu />
+        </header>
+    );
+}
