94c370254b071aa731772bd8d73136f74e647793
Floretcl <38957472+floretcl@users.noreply.github.com>
2024-06-08 12:12:56 +0200

    clean

--- portfolio-front/src/components/Menu.js
+++ portfolio-front/src/components/Menu.js
@@ -23,40 +23,37 @@ export default function Menu() {
     // TODO : href
     return (
         <nav className={`header__nav`}>
-            <div className={`menu ${isActive ? "menu--active" : ""} ${isOpen ? "menu--open" : "menu--close"}`}>
-                <ul className="menu__list">
-                    <li className="menu__link">
-                        <a href="#">Home</a>
-                        <hr className="menu__divider"/>
-                    </li>
-                    <li className="menu__link">
-                        <a href="#">About</a>
-                        <hr className="menu__divider"/>
-                    </li>
-                    <li className="menu__link">
-                        <a href="#">Projects</a>
-                        <hr className="menu__divider"/>
-                    </li>
-                    <li className="menu__link">
-                        <a href="#">Contact</a>
-                        <hr className="menu__divider"/>
-                    </li>
-                </ul>
-                <Mode/>
-            </div>
-            <button className="menu__button" type="button" onClick={toggleMenu}>
-                <div className="menu__button-lines">
-                    <div
-                        className={`menu__button-line-top ${isOpen ? "menu__button-line-top--open" : ""} ${isClosed ? "menu__button-line-top--close" : ""}`}>
-                    </div>
-                    <div
-                        className={`menu__button-line-middle ${isOpen ? "menu__button-line-middle--open" : ""} ${isClosed ? "menu__button-line-middle--close" : ""}`}>
-                    </div>
-                    <div
-                        className={`menu__button-line-bottom ${isOpen ? "menu__button-line-bottom--open" : ""} ${isClosed ? "menu__button-line-bottom--close" : ""}`}>
-                    </div>
+            <div
+                className={`menu ${isActive ? "menu--active" : ""} ${isOpen ? "menu--open" : "menu--close"} ${isClosed ? "menu--closed" : ""}`}>
+                <div className="menu__content">
+                    <ul className="menu__list">
+                        <li className="menu__link">
+                            <a href="#">Home</a>
+                            <hr className="menu__divider"/>
+                        </li>
+                        <li className="menu__link">
+                            <a href="#">About</a>
+                            <hr className="menu__divider"/>
+                        </li>
+                        <li className="menu__link">
+                            <a href="#">Projects</a>
+                            <hr className="menu__divider"/>
+                        </li>
+                        <li className="menu__link">
+                            <a href="#">Contact</a>
+                            <hr className="menu__divider"/>
+                        </li>
+                    </ul>
+                    <Mode/>
                 </div>
-            </button>
+                <button className="menu__button" type="button" onClick={toggleMenu}>
+                    <div className="menu__button-lines">
+                        <div className="menu__button-line-top"></div>
+                        <div className="menu__button-line-middle"></div>
+                        <div className="menu__button-line-bottom"></div>
+                    </div>
+                </button>
+            </div>
         </nav>
     );
 }
