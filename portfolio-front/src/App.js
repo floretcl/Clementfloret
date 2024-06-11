185cf3cef5db09663cf82acaf04e72829161cadc
Floretcl <38957472+floretcl@users.noreply.github.com>
2024-06-11 21:48:25 +0200

    add Cursor.js to App

--- portfolio-front/src/App.js
+++ portfolio-front/src/App.js
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Cursor from "./components/Cursor";
 
 function App() {
   return (
       <div className="app">
         <Header />
         <Footer />
+        <Cursor />
       </div>
   );
 }
