afa957ab988b2afccc094ce00f44c8eedac5f973
Floretcl <38957472+floretcl@users.noreply.github.com>
2024-06-18 21:12:05 +0200

    modify contact component, add style

--- portfolio-front/src/components/Contact.js
+++ portfolio-front/src/components/Contact.js
@@ -1,13 +1,14 @@
 export default function Contact() {
     return (
         <main className="contact">
-            <h1>Contact</h1>
-            <p>Send me an email directly on clement.floret@icloud.com</p>
-            <form>
+            <h1 className="contact__title">Contact</h1>
+            <p className="contact__infos">Send me an email directly on clement.floret@icloud.com</p>
+            <form className="contact__form">
                 <input type="text" id="name" name="name" minLength="3" maxLength="100" placeholder="Name" required/>
                 <input type="email" id="email" name="email" minLength="3" maxLength="254" placeholder="Email" required/>
-                <input type="text" id="subject" name="subject" minLength="5" maxLength="200" placeholder="Subject" required/>
-                <textarea id="message" name="message" minLength="10" placeholder="Message" required></textarea>
+                <input type="text" id="subject" name="subject" minLength="5" maxLength="200" placeholder="Subject"/>
+                <textarea id="message" name="message" minLength="10" placeholder="Message" rows="10" required></textarea>
+                <input type="submit" value="Submit"/>
             </form>
         </main>
     );
