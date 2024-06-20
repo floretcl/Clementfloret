b4f68f0f5f3c935a2ec7f56e9bec3f139cf792b6
Floretcl <38957472+floretcl@users.noreply.github.com>
2024-06-20 19:38:43 +0200

    modify projectCard component

--- portfolio-front/src/components/ProjectCard.js
+++ portfolio-front/src/components/ProjectCard.js
@@ -1,8 +1,10 @@
 export default function ProjectCard({name, imgSrc}) {
     return (
         <div className="project-card">
-            <div>
-                <img className="project-card__img" src={imgSrc} alt="{name} thumbnail"/>
+            <div className="project-card__thumbnail">
+                <div className="project-card__mask">
+                    <img className="project-card__img" src={imgSrc} alt="{name} thumbnail"/>
+                </div>
             </div>
             <p className="project-card__name">{name}</p>
         </div>
