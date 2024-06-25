87f91600573b17bfce0dde796550caa5d7738edb
Floretcl <38957472+floretcl@users.noreply.github.com>
2024-06-25 23:39:42 +0200

    replace projectCard img alt

--- portfolio-front/src/components/ProjectCard.js
+++ portfolio-front/src/components/ProjectCard.js
@@ -3,7 +3,7 @@ export default function ProjectCard({name, imgSrc}) {
         <div className="project-card">
             <div className="project-card__thumbnail">
                 <div className="project-card__mask">
-                    <img className="project-card__img" src={imgSrc} alt="{name} thumbnail"/>
+                    <img className="project-card__img" src={imgSrc} alt={name + " thumbnail"}/>
                 </div>
             </div>
             <p className="project-card__name">{name}</p>
