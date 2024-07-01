b47338e0e233a98f3ee0b037c802f4190af5b7cd
Floretcl <38957472+floretcl@users.noreply.github.com>
2024-07-01 20:46:52 +0200

    improve header and menu/nav

--- portfolio-front/src/components/Menu.js
+++ portfolio-front/src/components/Menu.js
@@ -2,19 +2,16 @@ import {useState} from "react";
 import Mode from "./Mode";
 
 export default function Menu() {
-    const [isClosed, setIsClosed] = useState(false);
     const [isActive, setIsActive] = useState(false);
     const [isOpen, setIsOpen] = useState(false);
 
     function toggleMenu() {
         if (isActive === true) {
-            setIsClosed(true);
             setIsOpen(false);
             setTimeout(() => {
                 setIsActive(false);
             }, 500);
         } else {
-            setIsClosed(false);
             setIsOpen(true);
             setIsActive(true);
         }
@@ -23,8 +20,16 @@ export default function Menu() {
     // TODO : href
     return (
         <div
-            className={`menu ${isActive ? "menu--active" : ""} ${isOpen ? "menu--open" : "menu--close"} ${isClosed ? "menu--closed" : ""}`}>
+            className={`menu ${isActive ? "menu--active" : ""} ${isOpen ? "menu--open" : "menu--close"}`}>
+            <button className="menu__button" type="button" onClick={toggleMenu}>
+                <div className="menu__button-lines">
+                    <div className="menu__button-line-top"></div>
+                    <div className="menu__button-line-middle"></div>
+                    <div className="menu__button-line-bottom"></div>
+                </div>
+            </button>
             <div className="menu__content">
+                <Mode/>
                 <ul className="menu__list">
                     <li className="menu__link">
                         <a href="#">Home</a>
@@ -43,15 +48,7 @@ export default function Menu() {
                         <hr className="menu__divider"/>
                     </li>
                 </ul>
-                <Mode/>
             </div>
-            <button className="menu__button" type="button" onClick={toggleMenu}>
-                <div className="menu__button-lines">
-                    <div className="menu__button-line-top"></div>
-                    <div className="menu__button-line-middle"></div>
-                    <div className="menu__button-line-bottom"></div>
-                </div>
-            </button>
         </div>
     );
 }
