e36d628c9e123552019765a0fc733cc78df3de81
Floretcl <38957472+floretcl@users.noreply.github.com>
2024-07-08 22:13:23 +0200

    modify project-card tag: div to a remove project-card mask

--- portfolio-front/src/components/ProjectCard.js
+++ portfolio-front/src/components/ProjectCard.js
@@ -1,15 +1,13 @@
 export default function ProjectCard({name, imgSrc, type}) {
     return (
-        <div className="project-card">
+        <a className="project-card" href="#">
             <div className="project-card__thumbnail">
-                <div className="project-card__mask">
-                    <img className="project-card__img" src={imgSrc} alt={name + " thumbnail"}/>
-                </div>
+                <img className="project-card__img" src={imgSrc} alt={name + " thumbnail"}/>
             </div>
             <div className="project-card__text">
                 <p className="project-card__name">{name}</p>
                 <p className="project-card__type">{type}</p>
             </div>
-        </div>
+        </a>
     );
 }
