dc4f44d3da2a78806849636363414dc92afb57a2
Floretcl <38957472+floretcl@users.noreply.github.com>
2024-07-24 19:54:20 +0200

    add target blank to ResumeButton link

--- portfolio-front/portfolio-react-app/src/components/ResumeButton/ResumeButton.jsx
+++ portfolio-front/portfolio-react-app/src/components/ResumeButton/ResumeButton.jsx
@@ -8,7 +8,7 @@ export default function ResumeButton({url}) {
                     d="M53.2975 133.488L256 21.6979L458.703 133.488V355.587L256 467.377L53.2975 355.587V133.488Z"
                     stroke="currentColor" strokeWidth="18"/>
             </svg>
-            <a className="hoverable" href={url}>
+            <a className="hoverable" href={url} target={"_blank"}>
                 Resume<br/>
                 Pdf
             </a>
