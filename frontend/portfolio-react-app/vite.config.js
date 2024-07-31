d1b55d94b15dcb59895475a052680509f0602b22
Floretcl <38957472+floretcl@users.noreply.github.com>
2024-07-31 20:42:12 +0200

    modify vite.config.js add build config

--- frontend/portfolio-react-app/vite.config.js
+++ frontend/portfolio-react-app/vite.config.js
@@ -1,7 +1,32 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import {defineConfig} from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [
+        react(),
+    ],
+    build: {
+        outDir: '../../backend/portfolio/static/portfolio',
+        assetsDir: '../../backend/portfolio/static/portfolio/assets',
+        emptyOutDir: true,
+        rollupOptions: {
+            output: {
+                chunkFileNames: 'assets/js/[name]-[hash].js',
+                entryFileNames: 'assets/js/[name]-[hash].js',
+                assetFileNames: ({name}) => {
+                    if (/\.(gif|jpe?g|png|svg|webp|avif)$/.test(name ?? '')) {
+                        return 'assets/img/[name]-[hash][extname]';
+                    }
+                    if (/\.(css|scss)$/.test(name ?? '')) {
+                        return 'assets/css/[name]-[hash][extname]';
+                    }
+                    if (/\.(woff|woff2|ttf|otf)$/.test(name ?? '')) {
+                        return 'assets/fonts/[name]-[hash][extname]';
+                    }
+                    return 'assets/[name]-[hash][extname]';
+                },
+            },
+        },
+    },
+});
