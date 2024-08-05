4382d157e27f278a51277a2b8e15e2746834a47f
Floretcl <38957472+floretcl@users.noreply.github.com>
2024-08-05 16:47:57 +0200

    modify auto serialized context data to manual

--- backend/portfolio/views.py
+++ backend/portfolio/views.py
@@ -3,7 +3,7 @@ from django.contrib import messages
 from django.views.generic import TemplateView, DetailView, FormView, ListView
 
 from portfolio.forms import ContactForm
-from portfolio.models import Portfolio, PortfolioLink, ProjectType, Project
+from portfolio.models import Portfolio, PortfolioLink, ProjectType, Project, ProjectLink, ProjectImage, Icon, Skill
 
 
 class IndexView(TemplateView):
@@ -11,8 +11,31 @@ class IndexView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['portfolio'] = serializers.serialize("json", Portfolio.objects.filter(active=True))
-        context['portfolio_links'] = serializers.serialize("json", PortfolioLink.objects.order_by('order'))
+        portfolio = Portfolio.objects.filter(active=True).first()
+        about_links = PortfolioLink.objects.filter(portfolio=portfolio).order_by('order')
+
+        portfolio_data = {
+            "id": portfolio.pk,
+            "name": portfolio.name,
+            "name_fr": portfolio.name_fr,
+            "firstname": portfolio.firstname,
+            "lastname": portfolio.lastname,
+            "job_title": portfolio.job_title,
+            "job_title_fr": portfolio.job_title_fr,
+            "contact_email": portfolio.contact_email,
+            "resume": portfolio.resume.url,
+        }
+        about_links_data = [
+            {
+                "id": link.pk,
+                "name": link.name,
+                "icon": link.icon.svg_file.url if link.icon else None,
+                "link": link.link,
+            }
+            for link in about_links
+        ]
+        context['portfolio'] = portfolio_data
+        context['about_links'] = about_links_data
         return context
 
 
@@ -21,8 +44,42 @@ class AboutView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['portfolio'] = serializers.serialize("json", Portfolio.objects.filter(active=True))
-        context['portfolio_links'] = serializers.serialize("json", PortfolioLink.objects.order_by('order'))
+        portfolio = Portfolio.objects.filter(active=True).first()
+        about_skills = Skill.objects.filter(portfolio=portfolio).order_by('order')
+        about_links = PortfolioLink.objects.filter(portfolio=portfolio).order_by('order')
+
+        portfolio_data = {
+            "pk": portfolio.pk,
+            "name": portfolio.name,
+            "name_fr": portfolio.name_fr,
+            "firstname": portfolio.firstname,
+            "lastname": portfolio.lastname,
+            "contact_email": portfolio.contact_email,
+            "avatar": portfolio.avatar.url,
+            "about_description": portfolio.about_description,
+            "about_description_fr": portfolio.about_description_fr,
+            "about_skill": list(portfolio.about_skill.values_list("id", flat=True)),
+        }
+        about_skills_data = [
+            {
+                "id": skill.pk,
+                "name": skill.name,
+                "icon": skill.icon.svg_file.url if skill.icon else None,
+            }
+            for skill in about_skills if skill.icon is not None
+        ]
+        about_links_data = [
+            {
+                "id": link.pk,
+                "name": link.name,
+                "icon": link.icon.svg_file.url if link.icon else None,
+                "link": link.link,
+            }
+            for link in about_links
+        ]
+        context['portfolio'] = portfolio_data
+        context['about_skills'] = about_skills_data
+        context['about_links'] = about_links_data
         return context
 
 
@@ -33,8 +90,47 @@ class ProjectListView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['project_types'] = serializers.serialize("json", ProjectType.objects.all())
-        context['portfolio_links'] = serializers.serialize("json", PortfolioLink.objects.order_by('order'))
+        portfolio = Portfolio.objects.filter(active=True).first()
+        about_links = PortfolioLink.objects.filter(portfolio=portfolio).order_by('order')
+        project_types = ProjectType.objects.filter(portfolio=portfolio).order_by()
+        projects = Project.objects.filter(portfolio=portfolio).order_by('order')
+
+        portfolio_data = {
+            "id": portfolio.pk,
+            "firstname": portfolio.firstname,
+            "lastname": portfolio.lastname,
+            "contact_email": portfolio.contact_email,
+        }
+        about_links_data = [
+            {
+                "id": link.pk,
+                "name": link.name,
+                "icon": link.icon.svg_file.url if link.icon else None,
+                "link": link.link,
+            }
+            for link in about_links
+        ]
+        project_types_data = [
+            {
+                "id": project_type.pk,
+                "name": project_type.name,
+                "name_fr": project_type.name_fr,
+            }
+            for project_type in project_types
+        ]
+        projects_data = [
+            {
+                "id": project.pk,
+                "name": project.name,
+                "name_fr": project.name_fr,
+                "project_type": project.project_type.name,
+            }
+            for project in projects
+        ]
+        context['portfolio'] = portfolio_data
+        context['about_links'] = about_links_data
+        context['project_types'] = project_types_data
+        context['projects'] = projects_data
         return context
 
 
@@ -44,8 +140,57 @@ class ProjectDetailView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['project'] = serializers.serialize('json', [self.object])
-        context['portfolio_links'] = serializers.serialize("json", PortfolioLink.objects.order_by('order'))
+        portfolio = Portfolio.objects.filter(active=True).first()
+        about_links = PortfolioLink.objects.filter(portfolio=portfolio).order_by('order')
+        project = self.object
+        project_images = ProjectImage.objects.filter(project=project).order_by()
+        project_links = ProjectLink.objects.filter(project=project).order_by('order')
+
+        portfolio_data = {
+            "id": portfolio.pk,
+            "firstname": portfolio.firstname,
+            "lastname": portfolio.lastname,
+            "contact_email": portfolio.contact_email,
+        }
+        about_links_data = [
+            {
+                "id": link.pk,
+                "name": link.name,
+                "icon": link.icon.svg_file.url if link.icon else None,
+                "link": link.link,
+            }
+            for link in about_links
+        ]
+        project_data = {
+            "id": project.pk,
+            "name": project.name,
+            "name_fr": project.name_fr,
+            "description": project.description,
+            "description_fr": project.description_fr,
+            "skills": list(project.skill.values_list("id", "name").order_by("order")),
+            "type": project.project_type.name,
+        }
+        project_images = [
+            {
+                "id": image.pk,
+                "name": image.name,
+                "image": image.image.url
+            }
+            for image in project_images
+        ]
+        project_links = [
+            {
+                "id": link.pk,
+                "name": link.name,
+                "icon": link.icon.svg_file.url if link.icon else None,
+            }
+            for link in project_links
+        ]
+        context['portfolio'] = portfolio_data
+        context['about_links'] = about_links_data
+        context['project'] = project_data
+        context['project_images'] = project_images
+        context['project_links'] = project_links
         return context
 
 
@@ -67,5 +212,29 @@ class ContactView(FormView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['portfolio_links'] = serializers.serialize("json", PortfolioLink.objects.order_by('order'))
+        portfolio = Portfolio.objects.filter(active=True).first()
+        about_links = PortfolioLink.objects.filter(portfolio=portfolio).order_by('order')
+
+        portfolio_data = {
+            "model": portfolio._meta.label_lower,
+            "pk": portfolio.pk,
+            "name": portfolio.name,
+            "name_fr": portfolio.name_fr,
+            "firstname": portfolio.firstname,
+            "lastname": portfolio.lastname,
+            "contact_email": portfolio.contact_email,
+            "about_link": list(portfolio.about_link.values_list("id", flat=True)),
+        }
+        about_links_data = [
+            {
+                "model": link._meta.label_lower,
+                "id": link.pk,
+                "name": link.name,
+                "icon": link.icon.svg_file.url if link.icon else None,
+                "link": link.link,
+            }
+            for link in about_links
+        ]
+        context['portfolio'] = portfolio_data
+        context['about_links'] = about_links_data
         return context
