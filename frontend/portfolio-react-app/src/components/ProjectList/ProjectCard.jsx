5d5f001ad6ea5ae3d3bc13286d5b07145a2a0ba3
Floretcl <38957472+floretcl@users.noreply.github.com>
2024-09-26 23:04:06 +0200

    start modify project list in vertical different carrousel

--- frontend/portfolio-react-app/src/components/ProjectList/ProjectCard.jsx
+++ frontend/portfolio-react-app/src/components/ProjectList/ProjectCard.jsx
@@ -1,16 +1,42 @@
 import PropTypes from "prop-types";
 import '../../styles/ProjectCard.scss'
 import {useTranslation} from "react-i18next";
+import {useEffect, useState} from "react";
 
-export default function ProjectCard({id, name, image, type}) {
+export default function ProjectCard({length, index, id, name, image, type}) {
     const {i18n, t } = useTranslation();
+    const [prev, setPrev] = useState(0);
+    const [next, setNext] = useState(0);
+
+    useEffect(() => {
+        const setPreviousItem = () => {
+            if (index !== 1) {
+                setPrev(index - 1);
+            } else {
+                setPrev(length);
+            }
+        }
+
+        const setNextItem = () => {
+            if (index !== length) {
+                setNext(index + 1);
+            } else {
+                setNext(1);
+            }
+        }
+
+        setPreviousItem();
+        setNextItem();
+
+    }, [index, length]);
 
     return (
-        <li>
+        <li className={`project-card-carrousel
+            ${id === prev? "project-card-carrousel--prev" : ""} 
+            ${id === index ? "project-card-carrousel--active" : ""} 
+            ${id === next ? "project-card-carrousel--next" : ""}`}>
             <a className="project-card hoverable" href={`/${i18n.language}/project/${id}`}>
-                <div className="project-card__thumbnail">
-                    <img className="project-card__img" src={image.url} alt={`${image.name} ${t('thumbnail')}`}/>
-                </div>
+                <img className="project-card__img" src={image.url} alt={`${image.name} ${t('thumbnail')}`}/>
                 <div className="project-card__text">
                     <p className="project-card__name">{name}</p>
                     <p className="project-card__type">{type}</p>
@@ -21,6 +47,8 @@ export default function ProjectCard({id, name, image, type}) {
 }
 
 ProjectCard.propTypes = {
+    length: PropTypes.number.isRequired,
+    index: PropTypes.number.isRequired,
     id: PropTypes.number.isRequired,
     name: PropTypes.string.isRequired,
     image: PropTypes.object.isRequired,
--- frontend/portfolio-react-app/src/components/ProjectList/ProjectList.jsx
+++ frontend/portfolio-react-app/src/components/ProjectList/ProjectList.jsx
@@ -1,16 +1,27 @@
 import ProjectCard from "./ProjectCard.jsx";
-import {useRef} from "react";
+import {useEffect, useMemo, useRef, useState} from "react";
 import PropTypes from "prop-types";
 import '../../styles/ProjectList.scss'
 
 export default function ProjectList({filter}) {
     const contentRef = useRef(null);
+    const [index, setIndex] = useState(1);
+    const [count, setCount] = useState(0);
+
     const projects = JSON.parse(document.getElementById("projects").textContent);
+
+    const items = useMemo(() => {
+        return projects;
+    }, [projects]);
+
+    const itemsLength = useMemo(() => {
+        return projects.length;
+    }, [projects]);
 
     const projectsFiltered = projectFiltration(projects)
 
     const listProjects = projectsFiltered.map(project =>
-        <ProjectCard key={project.id} id={project.id} name={project.name} image={project.images[0]} type={project.type} />
+        <ProjectCard length={itemsLength} index={index} key={project.id} id={project.id} name={project.name} image={project.images[0]} type={project.type} />
     );
 
     function projectFiltration(projects) {
@@ -21,6 +32,15 @@ export default function ProjectList({filter}) {
         }
     }
 
+    function previousItem() {
+        setIndex((index) => (index === 1 ? items.length : index - 1));
+    }
+
+    function nextItem() {
+        setIndex((index) => (index === items.length ? 1 : index + 1));
+    }
+
+    /*
     function handleBackButton() {
         contentRef.current.scrollLeft -= 100;
     }
@@ -28,13 +48,43 @@ export default function ProjectList({filter}) {
     function handleNextButton() {
         contentRef.current.scrollLeft += 100;
     }
+     */
+
+    useEffect(() => {
+        let startCarousel = false;
+        const interval = setInterval(() => {
+            setCount(prevState => (prevState + 1) % itemsLength);
+            startCarousel = true;
+        }, 3000);
+
+        if (startCarousel) {
+            setIndex((index) => (index === itemsLength ? 1 : index + 1));
+        }
+
+        return () => {
+            clearInterval(interval);
+        }
+    }, [count, itemsLength]);
 
 
     return (
         <>
             <div className="projects-list__buttons">
-                <button className="projects-list__button hoverable" onClick={handleBackButton}>&lsaquo;</button>
-                <button className="projects-list__button hoverable" onClick={handleNextButton}>&rsaquo;</button>
+                <button className="projects-list__button hoverable" onClick={previousItem}>
+                    <svg width="512" height="512" viewBox="0 0 512 512" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M255.998 231.006L150.404 336.602L120.234 306.432L255.998 170.667L391.763 306.432L361.594 336.602L255.998 231.006Z"
+                            fill="currentColor"/>
+                    </svg>
+
+                </button>
+                <button className="projects-list__button hoverable" onClick={nextItem}>
+                    <svg width="512" height="512" viewBox="0 0 512 512" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M256 281.003L361.6 175.403L391.765 205.568L256 341.333L120.235 205.568L150.4 175.403L256 281.003Z"
+                            fill="currentColor"/>
+                    </svg>
+                </button>
             </div>
             <div ref={contentRef} className="projects-list__content">
                 <ol className="projects-list__list">
