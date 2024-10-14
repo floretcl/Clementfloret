f77d5b77b27a237e5de40f35bcf59519372d4efe
Floretcl <38957472+floretcl@users.noreply.github.com>
2024-10-14 22:28:59 +0200

    new carrousel for project list

--- frontend/portfolio-react-app/src/components/ProjectList/ProjectList.jsx
+++ frontend/portfolio-react-app/src/components/ProjectList/ProjectList.jsx
@@ -1,17 +1,51 @@
-import ProjectCard from "./ProjectCard.jsx";
-import {useRef} from "react";
 import PropTypes from "prop-types";
 import '../../styles/ProjectList.scss'
+import {useRef, useState} from "react";
+import ProjectCard from "./ProjectCard.jsx";
 
 export default function ProjectList({filter}) {
     const contentRef = useRef(null);
+    const pointerStartX = useRef(0);
+    const pointerEndX = useRef(0);
+    const isSwiping = useRef(false);
+    const [index, setIndex] = useState(1);
+    
     const projects = JSON.parse(document.getElementById("projects").textContent);
 
-    const projectsFiltered = projectFiltration(projects)
+    const projectsFiltered = projectFiltration(projects);
+    const projectsLength = projectsFiltered.length;
 
-    const listProjects = projectsFiltered.map(project =>
-        <ProjectCard key={project.id} id={project.id} name={project.name} image={project.images[0]} type={project.type} />
-    );
+    const listProjects = projectsFiltered.map((project,i) => {
+        i++;
+
+        const className = (i) => {
+            if (i === index) {
+                return "project-card--index";
+            } else if (i === getIndexPlus2() && projectsLength >= 5) {
+                return "project-card--indexPlus2";
+            } else if (i === getIndexPlus1() && projectsLength >= 3) {
+                return "project-card--indexPlus1";
+            } else if (i === getIndexMinus1() && projectsLength >= 3) {
+                return "project-card--indexMinus1";
+            } else if (i === getIndexMinus2() && projectsLength >= 5) {
+                return "project-card--indexMinus2";
+            } else {
+                return "";
+            }
+        }
+
+        return <ProjectCard key={i}
+                            id={i}
+                            name={project.name}
+                            image={project.images[0]}
+                            type={project.type}
+                            className={className(i)}
+                            onDragStart={(e) => e.preventDefault()}
+                            onPointerDown={handlePointerDown}
+                            onPointerUp={handlePointerUp}
+                            onPointerMove={handlePointerMove}
+                            onClick={handleClick}/>
+    });
 
     function projectFiltration(projects) {
         if (filter !== "") {
@@ -21,20 +55,107 @@ export default function ProjectList({filter}) {
         }
     }
 
-    function handleBackButton() {
-        contentRef.current.scrollLeft -= 100;
+    function getIndexMinus2() {
+        if (index - 2 > 0) {
+            return (index - 2);
+        } else {
+            return (((index - 2) % projectsLength) + projectsLength);
+        }
     }
 
-    function handleNextButton() {
-        contentRef.current.scrollLeft += 100;
+    function getIndexMinus1() {
+        if (index - 1 > 0) {
+            return (index - 1);
+        } else {
+            return (((index - 1) % projectsLength) + projectsLength);
+        }
     }
 
+    function getIndexPlus1() {
+        if (index + 1 > projectsLength) {
+            return ((index + 1) % projectsLength);
+        } else {
+            return (index + 1);
+        }
+    }
+
+    function getIndexPlus2() {
+        if (index + 2 > projectsLength) {
+            return ((index + 2) % projectsLength);
+        } else {
+            return (index + 2);
+        }
+    }
+
+    function handlePointerDown(e) {
+        pointerStartX.current = e.clientX;
+    }
+
+    function handlePointerUp() {
+        const movementX = pointerEndX.current - pointerStartX.current;
+        if (Math.abs(movementX) > 40 && window.innerWidth >= 640) {
+            isSwiping.current = true;
+            if (movementX < 0) {
+                nextItem();
+            } else {
+                previousItem();
+            }
+        }
+    }
+
+    function handlePointerMove(e) {
+        pointerEndX.current = e.clientX;
+    }
+
+    function handleClick(e) {
+        if (isSwiping.current) {
+            e.preventDefault();
+        }
+    }
+
+    function previousItem() {
+        setIndex((index) => (index === 1 ? projectsLength : index - 1));
+    }
+
+    function nextItem() {
+        setIndex((index) => (index === projectsLength ? 1 : index + 1));
+    }
 
     return (
         <>
-            <div className="projects-list__buttons">
-                <button className="projects-list__button hoverable" onClick={handleBackButton}>&lsaquo;</button>
-                <button className="projects-list__button hoverable" onClick={handleNextButton}>&rsaquo;</button>
+            <div className="projects-list__buttons-yaxis">
+                <button className="projects-list__button hoverable" onClick={previousItem}>
+                    <svg width="512" height="512" viewBox="0 0 512 512" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M255.998 231.006L150.404 336.602L120.234 306.432L255.998 170.667L391.763 306.432L361.594 336.602L255.998 231.006Z"
+                            fill="currentColor"/>
+                    </svg>
+                </button>
+                <button className="projects-list__button hoverable" onClick={nextItem}>
+                    <svg width="512" height="512" viewBox="0 0 512 512" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M256 281.003L361.6 175.403L391.765 205.568L256 341.333L120.235 205.568L150.4 175.403L256 281.003Z"
+                            fill="currentColor"/>
+                    </svg>
+                </button>
+            </div>
+            <div className="projects-list__buttons-xaxis">
+                <button className="projects-list__button hoverable" onClick={previousItem}>
+                    <svg width="512" height="512" viewBox="0 0 512 512" fill="none"
+                         xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M231.006 256.015L336.602 361.608L306.432 391.778L170.667 256.015L306.432 120.25L336.602 150.42L231.006 256.015Z"
+                            fill="currentColor"/>
+                    </svg>
+                </button>
+                <button className="projects-list__button hoverable" onClick={nextItem}>
+                    <svg width="512" height="512" viewBox="0 0 512 512" fill="none"
+                         xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M280.996 256.015L175.401 150.42L205.571 120.25L341.335 256.015L205.571 391.778L175.401 361.608L280.996 256.015Z"
+                            fill="currentColor"/>
+                    </svg>
+                </button>
             </div>
             <div ref={contentRef} className="projects-list__content">
                 <ol className="projects-list__list">
