197356bb9aa40b154969472ce292359117f58954
Floretcl <38957472+floretcl@users.noreply.github.com>
2024-11-01 15:02:29 +0100

    replace path with re_path for regex use in index main url

--- backend/portfolio/urls.py
+++ backend/portfolio/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from .views import (
     IndexView,
@@ -13,7 +13,7 @@ from .views import (
 
 app_name = 'portfolio'
 urlpatterns = [
-    path(r'^.*$', IndexView.as_view(), name='index'),
+    re_path(r'^.*$', IndexView.as_view(), name='index'),
     path('api/portfolio', PortfolioView.as_view(), name='about'),
     path('api/portfolio_links', PortfolioLinksView.as_view(), name='portfolio-links'),
     path('api/portfolio_skills', PortfolioSkillsView.as_view(), name='portfolio-skills'),
