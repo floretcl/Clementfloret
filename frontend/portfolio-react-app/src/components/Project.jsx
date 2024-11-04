399ec9fb20f0acb6a07f3526da3075290aca0b2f
Floretcl <38957472+floretcl@users.noreply.github.com>
2024-11-04 19:57:59 +0100

    fix last merge, branch fetch

--- frontend/portfolio-react-app/src/components/Project.jsx
+++ frontend/portfolio-react-app/src/components/Project.jsx
@@ -2,29 +2,37 @@ import Carousel from "./Carousel/Carousel.jsx";
 import ProjectSkill from "./Project/ProjectSkill.jsx";
 import ProjectLink from "./Project/ProjectLink.jsx";
 import '../styles/Project.scss'
-import {useEffect, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 
 export default function Project() {
-    const [project, setProject] = useState({});
-
-    const listLinks = project.links.map((link) =>
-        <ProjectLink key={link.id} url={link.url} name={link.name} icon={link.icon}/>
-    );
-
-    let listSkills = project.skills.map((skill) => {
-        return <ProjectSkill key={skill[0]} name={skill[1]}/>;
-    });
-
-    // add "-" between each ProjectSkill
-    listSkills = listSkills.reduce((accumulator, elements) => {
-        return accumulator === null ? [elements] : [...accumulator, "-", elements]
-    }, null);
+    const [project, setProject] = useState([]);
+    const [images, setImages] = useState([]);
+    const [listSkills, setListSkills] = useState([]);
+    const [listLinks, setListLinks] = useState([]);
 
     useEffect(() => {
+        function getListLinks() {
+            return project.links.map((link) =>
+                <ProjectLink key={link.id} url={link.url} name={link.name} icon={link.icon}/>
+            )
+        }
+
+        function getListSkills() {
+            let list = project.skills.map((skill) =>
+                <ProjectSkill key={skill[0]} name={skill[1]}/>
+            )
+            // add "-" between each ProjectSkill
+            return list.reduce((accumulator, elements) => {
+                return accumulator === null ? [elements] : [...accumulator, "-", elements];
+            }, null);
+        }
+
         function fetchRequest() {
             const pathName = window.location.pathname;
+            const pathNameSplit = pathName.split('/');
+            const id = pathNameSplit[pathNameSplit.length - 1];
             const langPrefix = pathName.startsWith('/fr/') ? '/fr' : pathName.startsWith('/en/') ? '/en' : '';
-            const url = `${langPrefix}/api/project`;
+            const url = `${langPrefix}/api/project?id=${id}`;
 
             const init = {
                 method: "GET",
@@ -39,17 +47,21 @@ export default function Project() {
                 .then(response => response.json())
                 .then(data => {
                     setProject(data);
+                    setListSkills(getListSkills());
+                    setListLinks(getListLinks());
+                    setImages(project.images);
                 })
                 .catch(error => {
                     console.log(`Error getting project data: ${error}`);
                 });
         }
+
         fetchRequest();
     }, [project]);
 
     return (
         <main className="project">
-            <Carousel images={project.images}/>
+            <Carousel images={images}/>
             <div className="project__content">
                 <h1 className="project__title">{project.name}</h1>
                 <p className="project__text">
