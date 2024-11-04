b72d33b30de3872a3ad6a31bfa89c3aa22fbb44e
Floretcl <38957472+floretcl@users.noreply.github.com>
2024-11-04 19:29:30 +0100

    fix issue : move regex path to end

--- backend/portfolio/urls.py
+++ backend/portfolio/urls.py
@@ -12,8 +12,8 @@ from .views import (
 )
 
 app_name = 'portfolio'
+
 urlpatterns = [
-    re_path(r'^.*$', IndexView.as_view(), name='index'),
     path('api/portfolio', PortfolioView.as_view(), name='about'),
     path('api/portfolio_links', PortfolioLinksView.as_view(), name='portfolio-links'),
     path('api/portfolio_skills', PortfolioSkillsView.as_view(), name='portfolio-skills'),
@@ -21,4 +21,5 @@ urlpatterns = [
     path('api/projects', ProjectsView.as_view(), name='projects'),
     path('api/project', ProjectView.as_view(), name='project'),
     path('api/contact', ContactView.as_view(), name='contact'),
+    re_path(r'^.*$', IndexView.as_view(), name='index'),
 ]
