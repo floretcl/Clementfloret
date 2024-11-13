f6ea1e0f203d4ff561d288b8ebe8db35263355c3
Floretcl <38957472+floretcl@users.noreply.github.com>
2024-11-13 23:35:45 +0100

    exclude projects filter to scroll reset

--- frontend/portfolio-react-app/src/ScrollToTop.jsx
+++ frontend/portfolio-react-app/src/ScrollToTop.jsx
@@ -5,7 +5,14 @@ export function ScrollToTop() {
         const {pathname} = useLocation();
 
         useLayoutEffect(() => {
+            const regexp = new RegExp('[a-z]{2}/projects/.');
+
             // Scroll to the top of the page when the route changes
-            window.scrollTo({top: 0, left: 0, behavior: 'instant'});
+            // except with project filters
+            if (!regexp.test(pathname)) {
+                window.scrollTo({top: 0, left: 0, behavior: 'instant'});
+            }
         }, [pathname]);
+
+        return null;
 }
